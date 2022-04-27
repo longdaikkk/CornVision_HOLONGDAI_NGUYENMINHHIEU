0c6e861c3c273b946074ebf2a9b6f34ab7a13e98
longdaikkk <holongdai@gmail.com>
2022-04-27 11:37:07 +0700

    responsive

--- main.js
+++ main.js
@@ -7,3 +7,11 @@ document.getElementById("close__btn").onclick=function(){
     document.getElementById("search__text").style.display = "none";
     document.getElementById("btn__search").className = "btn btn-success search_style_in";
 }
+
+document.getElementById("bars__toggler").onclick = function(){
+    if(document.getElementById("bars").className == "fa-solid fa-x text-light"){
+        document.getElementById("bars").className = "fa-solid fa-bars text-light";
+    }else{
+        document.getElementById("bars").className = "fa-solid fa-x text-light";
+    }
+}
