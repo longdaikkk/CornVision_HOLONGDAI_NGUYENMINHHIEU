9d99510af2ce834f21ed18365920f7377411d4eb
longdaikkk <holongdai@gmail.com>
2022-05-01 02:38:27 +0700

    link light mode with dark mode

--- capstone_dark/main.js
+++ capstone_dark/main.js
@@ -5,3 +5,13 @@ window.onscroll = function() {
         document.getElementById("header").classList.remove("header-scroll");
     }
 }
+
+document.getElementById("btn__search").onclick=function(){
+    document.getElementById("btn__search").className = "btn btn-success search__style";
+    document.getElementById("search__text").style.display = "block";
+}
+
+document.getElementById("close__btn").onclick=function(){
+    document.getElementById("search__text").style.display = "none";
+    document.getElementById("btn__search").className = "btn btn-success search_style_in";
+}
